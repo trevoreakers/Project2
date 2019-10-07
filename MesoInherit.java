3e4cd006704af4f2d568e0bd6c16270b7ce3ee1e
Trevor Akers <trevor.e.akers-1@ou.edu>
2019-10-06 21:57:51 -0500

    Add private variable to hold stID

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,4 +1,5 @@
 
-public class MesoInherit extends MesoAbstract{
+public class MesoInherit extends MesoAbstract {
 	
+	private String stId; // Creates a variable to hold the stID from MesoStation. 
 }
