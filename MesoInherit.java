a0b1c8be0e0fd1cd3a50c47ce42fd7679d6f9612
Trevor Akers <trevor.e.akers-1@ou.edu>
2019-10-06 22:39:25 -0500

    Add method readFile and updated constructor

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader; // Imports BufferedReader class.
+import java.io.IOException;
 
 public class MesoInherit extends MesoAbstract {
 	
@@ -5,7 +7,19 @@ public class MesoInherit extends MesoAbstract {
 		
 	// Constructor. Gets the stID from the object passed from MesoStation.
 	public MesoInherit(MesoStation meso) {
-		this.stId = meso.getStID();
+		this.stId = meso.getStID(); // Sets the stId variable equal to the stID from MesoStation.
+		
+		// Tries to call the readFile method to read in station Ids from Mesonet.txt.
+		try {
+			readFile();
+		} catch (IOException e) {
+			e.getMessage();
+		}
+	}
+	
+	// Method to read in the station Ids from Mesonet and add them into an array.
+	public void readFile() throws IOException {
+		
 	}
 	
 	// Getter method for the stId.
