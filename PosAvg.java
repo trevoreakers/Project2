a10d654eb472a31350539f901a3cd8e79c77606f
Trevor Akers <trevor.e.akers-1@ou.edu>
2019-10-06 23:17:28 -0500

    Add stationId variable, added constructor, and getter method for stationId

--- PosAvg.java
+++ PosAvg.java
@@ -1,4 +1,15 @@
 
 public class PosAvg {
+
+	private String STiD; // Creates a variable to hold the stationId passed in through the constructor. 
 	
+	// ConStructor.
+	public PosAvg(String station) {
+		this.STiD = station;
+	}
+	
+	// Getter method for the stationId. 
+	public String getSTiD() {
+		return STiD;
+	}
 }
