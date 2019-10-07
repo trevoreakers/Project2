7e4abc0399086e06aa6014e75dac945c36a2f394
Trevor Akers <trevor.e.akers-1@ou.edu>
2019-10-06 22:32:44 -0500

    Add getter method for stId variable

--- MesoInherit.java
+++ MesoInherit.java
@@ -2,9 +2,14 @@
 public class MesoInherit extends MesoAbstract {
 	
 	private String stId; // Creates a variable to hold the stID from MesoStation. 
-	
+		
 	// Constructor. Gets the stID from the object passed from MesoStation.
 	public MesoInherit(MesoStation meso) {
 		this.stId = meso.getStID();
+	}
+	
+	// Getter method for the stId.
+	public String getId() {
+		return stId;
 	}
 }
