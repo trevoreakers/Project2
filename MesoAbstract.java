65a9c661b689277340837df46a0f7909261f6382
Trevor Akers <trevor.e.akers-1@ou.edu>
2019-10-06 21:32:49 -0500

    Add abstract method letterAverage w/return type char

--- MesoAbstract.java
+++ MesoAbstract.java
@@ -7,5 +7,6 @@ public abstract class MesoAbstract
 	//Abstract method calAverage to return integer array.
 	abstract int[] calAverage();
 
-	//TODO: Create an abstract method letterAverage with return type char.
+	//Abstract method letterAverage with return type char.
+	abstract char letterAverage();
 }
