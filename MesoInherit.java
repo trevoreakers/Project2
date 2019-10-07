c58c887531babbf1a57b288c95b974d07643b656
Trevor Akers <trevor.e.akers-1@ou.edu>
2019-10-06 22:02:45 -0500

    Add constructor

--- MesoInherit.java
+++ MesoInherit.java
@@ -2,4 +2,9 @@
 public class MesoInherit extends MesoAbstract {
 	
 	private String stId; // Creates a variable to hold the stID from MesoStation. 
+	
+	// Constructor. Gets the stID from the object passed from MesoStation.
+	public MesoInherit(MesoStation meso) {
+		this.stId = meso.getStID();
+	}
 }
