ffe903dd2c8092a5d7f8b335a7fa3ce575e7e65c
Trevor Akers <trevor.e.akers-1@ou.edu>
2019-10-06 21:28:03 -0500

    Adding MesoAbstract Class

---
+++ MesoAbstract.java
@@ -0,0 +1,11 @@
+/**
+ * @author Mohammad Mukhtaruzzaman
+ * @version 2019-09-18
+*/
+public abstract class MesoAbstract 
+{
+	//Abstract method calAverage to return integer array.
+	abstract int[] calAverage();
+
+	//TODO: Create an abstract method letterAverage with return type char.
+}
