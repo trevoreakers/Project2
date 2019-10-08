fda5382a8fe3b95a8844caac21d57cea591600e2
Trevor Akers <trevor.e.akers-1@ou.edu>
2019-10-07 20:42:07 -0500

    Updated the calAverage method

--- MesoInherit.java
+++ MesoInherit.java
@@ -6,12 +6,25 @@ public class MesoInherit extends MesoAbstract {
 	// Constructor. Gets the stID from the object passed from MesoStation.
 	public MesoInherit(MesoStation meso) {
 		this.stId = meso.getStID(); // Sets the stId variable equal to the stID from MesoStation.
-		
 	}
 	
+	// Method to find the ceiling, floor, and average of the station IDs in ascii form. 
 	protected int [] calAverage() {
-		int [] number = new int[5];
-		return number;
+		int [] ascii = new int[3];
+		int asciiValue = 0; 
+		for(int i = 0; i < stId.length(); ++i) {
+			asciiValue += stId.charAt(i);
+		}
+		double asciiAverage = (double) asciiValue / stId.length();
+		ascii[0] = (int) Math.ceil(asciiAverage);
+		ascii[1] = (int) Math.floor(asciiAverage);
+		if(asciiAverage < 0.5) {
+			ascii[2] = ascii[1];
+		} else if (asciiAverage >= 0.5) {
+			ascii[2] = ascii[0];
+		}
+		
+		return ascii;
 	}
 	
 	protected char letterAverage() {
