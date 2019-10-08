4ce309bc69169e80b40ab718a5fb1a8dda018fd2
Trevor Akers <trevor.e.akers-1@ou.edu>
2019-10-07 21:15:01 -0500

    Added getter method for variable letterAverage

--- LetterAvg.java
+++ LetterAvg.java
@@ -1,4 +1,15 @@
 
 public class LetterAvg {
-
+	
+	char letterAverage; // Holds the letter average passed in from mesoInherit.
+	
+	// Constructor.
+	public LetterAvg(char character) {
+		this.letterAverage = character;
+	}
+	
+	// Getter method for the letterAvg.
+	public char getLetterAverag() {
+		return letterAverage;
+	}
 }
